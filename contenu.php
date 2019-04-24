9b74a014770b1bb3f2a580e1019696a0ae0f901f
Soufian <soufianelhazman@gmail.com>
2019-04-24 09:42:50 +0200

    uptdate

--- contenu.php
+++ contenu.php
@@ -1,32 +1,32 @@
 <?php
-if (isset($_POST['ajout']) && !empty($_POST['ajout'])) {
+if (isset($_POST['add']) && !empty($_POST['add'])) {
     if (!empty($_POST['task'])) {
-        $task = $_POST['task'];
+    $task = $_POST['task'];
+
+    //trim
+    $task = trim($_POST['task']);
+
+    //sanitisation 
+    $sanitisation = array('task' => FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS,);
+    $result = filter_input_array(INPUT_POST, $sanitisation);
+
+    //Si le résultat retourné après le filtre est vide ou erreur
+    if (isset($task)) {
+    //--Récupérer le fichier JSON
+    $show = file_get_contents("assets/json/todo.json");
+    echo $show;
     
-        //trim
-        $task = trim($_POST['task']);
-
-        //sanitisation 
-        $sanitisation = array('task' => FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS,);
-        $result = filter_input_array(INPUT_POST, $sanitisation);
-
-        //Si le résultat retourné après le filtre est vide ou erreur
-        if (isset($task)) {
-       
-            //--Récupérer le fichier JSON
-            $todo = file_get_contents("assets/todo.json");
-            
-            //--Décoder le fichier JSON en PHP
-            $decode = json_decode($todo, true);
-            
-            //Créer un tableau en php
-            $table = array("tache" => $task);
-            
-            //you can turn PHP into JSON with json_encode()
-            $encode = json_encode($table);
-            
-
-            $put = file_put_contents("assets/todo.json", $encode, LOCK_EX);
-        }
+    //--Décoder le fichier JSON en PHP
+    $decode = json_decode($todo, true);
+    
+    //Créer un tableau en php
+    $table = array("task" => $task);
+    
+    //you can turn PHP into JSON with json_encode()
+    $encode = json_encode($table);
+    $put = file_put_contents("assets/json/todo.json", $encode, LOCK_EX);
     }
-}
+    }
+} ?>
+
+
